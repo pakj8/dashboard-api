1b018435988b6f604f4f70ed8a2359d0b00a0336
Prakash Jha <pjha.prakash0812@gmail.com>
2023-10-04 01:15:14 +0530

    my second commit

--- server.js
+++ server.js
@@ -4,15 +4,23 @@ const app = Express();
 const jsonRoute = require("./models/jsonData");
 const cors = require("cors");
 
-const MONGODB = `mongodb+srv://poojapithva2002:pooja1234@cluster0.lc5zdxk.mongodb.net/?retryWrites=true&w=majority`;
+const MONGODB = `mongodb://127.0.0.1:27017/jsondata`;
+// const MONGODB = `mongodb+srv://poojapithva2002:pooja1234@cluster0.lc5zdxk.mongodb.net/?retryWrites=true&w=majority`;
 app.use(cors());
 
 app.use(Express.json());
 
-mongoose.connect(MONGODB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Mongodb Connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.use("/api/jsondata", jsonRoute);
 
